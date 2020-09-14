25b98d41af5d1c7920cd7e2dcf009ef9aea4496c
Tae Perez <pereztae@knights.ucf.edu>
2020-09-14 17:33:37 -0400

    Application changes after Encrypter

--- perez_p1/src/Application.java
+++ perez_p1/src/Application.java
@@ -1,2 +1,7 @@
 public class Application {
+    public static void main (String[] args) {
+        Encrypter e = new Encrypter();
+        String str = e.encrypt("6789");
+        System.out.printf("%s", str);
+    }
 }
