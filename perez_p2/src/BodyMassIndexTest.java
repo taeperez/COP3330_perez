04ec592d8c8cbf83a83ecf6b1cdcc94cdad7e8b4
Tae Perez <pereztae@knights.ucf.edu>
2020-09-28 18:58:37 -0400

    Test cases written

--- perez_p2/src/BodyMassIndexTest.java
+++ perez_p2/src/BodyMassIndexTest.java
@@ -1,2 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BodyMassIndexTest {
+    @Test
+    // test for underweight bmi
+    public void testBodyMassIndexCalculationUnderweight() {
+        BodyMassIndex bmi = new BodyMassIndex (72.0, 100.0);
+        assertEquals(13.560956790123457, bmi.calculate_bmi(72.0, 100.0));
+    }
+    @Test
+    // test for normal weight bmi
+    public void testBodyMassIndexCalculationNormalWeight() {
+        BodyMassIndex bmi = new BodyMassIndex (70.0 ,170.0);
+        assertEquals(24.389795918367348, bmi.calculate_bmi(70.0,170.0));
+    }
+    @Test
+    // test for overweight bmi
+    public void testBodyMassIndexCalculationOverweight() {
+        BodyMassIndex bmi = new BodyMassIndex(72.0, 210.0);
+        assertEquals(28.47800925925926, bmi.calculate_bmi(72.0, 210.0));
+    }
+    @Test
+    // test for obese bmi
+    public void testBodyMassIndexCalculationObese() {
+        BodyMassIndex bmi = new BodyMassIndex(72.0, 250.0);
+        assertEquals(33.90239197530864, bmi.calculate_bmi(72.0, 250.0));
+    }
+    @Test
+    // test for underweight category
+    public void testBodyMassIndexCategoryUnderweight() {
+        BodyMassIndex bmi = new BodyMassIndex(72.0, 100.0);
+        assertEquals("Underweight", bmi.category(13.560956790123457));
+    }
+    @Test
+    // test for normal weight category
+    public void testBodyMassIndexCategoryNormalWeight() {
+        BodyMassIndex bmi = new BodyMassIndex(70.0, 170.0);
+        assertEquals("Normal weight", bmi.category(24.389795918367348));
+    }
+    @Test
+    // test for overweight category
+    public void testBodyMassIndexCategoryOverweight() {
+        BodyMassIndex bmi = new BodyMassIndex(72.0, 210.0);
+        assertEquals("Overweight", bmi.category(28.47800925925926));
+    }
+    @Test
+    // test for obese category
+    public void testBodyMassIndexCategoryObese() {
+        BodyMassIndex bmi = new BodyMassIndex(72.0, 250.0);
+        assertEquals("Obese", bmi.category(33.90239197530864));
+    }
 }
