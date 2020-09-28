959e933229abf0a1be3cf6589f3965805081a3ec
Tae Perez <pereztae@knights.ucf.edu>
2020-09-28 14:43:10 -0400

    Creation of moreinput() method

--- perez_p2/src/App.java
+++ perez_p2/src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
@@ -13,5 +15,17 @@ public class App {
         }
 
         displayBmiStatistics(bmiData);
+    }
+    public static boolean moreInput(){
+        System.out.println("Would you like to calculate another BMI? Type 'Y' for yes, or type 'N' for no.");
+        Scanner another_input = new Scanner(System.in);
+        char c = another_input.next().charAt(0);
+        if (c == 'Y') {
+            return true;
+        }
+        else if (c == 'N'){
+            return false;
+        }
+        return false; // if user did not input either a 'Y' or an 'N', it is assumed that they do not want to continue
     }
 }
