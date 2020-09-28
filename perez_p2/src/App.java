05a54564e9f6bdd0d1681a02bc498bcd3b44434c
Tae Perez <pereztae@knights.ucf.edu>
2020-09-28 13:41:55 -0400

    Added main method

--- perez_p2/src/App.java
+++ perez_p2/src/App.java
@@ -1,2 +1,17 @@
 public class App {
+    public static void main(String[] args) {
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while (moreInput()) {
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+    }
 }
