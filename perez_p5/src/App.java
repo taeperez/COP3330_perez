fede34232b6e739e77d5e69e21408cd46c7c26bc
Tae Perez <pereztae@knights.ucf.edu>
2020-12-01 21:09:06 -0500

    Initial configurations of app and tasklist classes

--- perez_p5/src/App.java
+++ perez_p5/src/App.java
@@ -1,2 +1,8 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class App {
+    private static final Scanner input = new Scanner(System.in);
+
+    private TaskList taskList;
+    private ContactList contactList;
 }
